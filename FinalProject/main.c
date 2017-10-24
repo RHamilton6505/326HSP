def8131a32706a1229666fe62b9f6e489beef4a0
ilikeschecters <hamiltia@mail.gvsu.edu>
2017-10-24 02:03:07 -0400

    added buzzer and LED functionality

--- FinalProject/main.c
+++ FinalProject/main.c
@@ -46,6 +46,8 @@
 static volatile uint16_t curADCResult;
 static volatile float normalizedADCRes = 3;
 int flag = 1;
+int alarmState = 2;
+int buzzCount = 0;
 
 
 int main(void)
@@ -54,11 +56,24 @@ int main(void)
     sysTimerInit();
     ADCInit();
     T32Init();
+    mainInit();
 
 
     while(1){
     }
     
+}
+
+void mainInit(){
+    //BUZZER 2.7
+    P2->DIR |=  BIT0 | BIT1 | BIT2;
+
+
+    //SCREEN 7.7
+    P7->DIR |= BIT7 | BIT6;
+    P7->SEL0 |= BIT7 | BIT6;
+    P7->SEL1 &= ~BIT7 | BIT6;
+
 }
 
 void ADC14_IRQHandler(void)
@@ -74,30 +89,57 @@ void ADC14_IRQHandler(void)
 }
 
 void T32_INT1_IRQHandler(void){
-    
     fflush(stdout);
-    //printf("T32 works\n");
+    printf("T32 works\n");
+
 
     int duty = (int) ((normalizedADCRes-.80)*50);
-    LED_PWM(50);
+    int dutyBuzz = 0;
+
     MAP_ADC14_toggleConversionTrigger();
+
+    if(alarmState == 0){
+        P2OUT &= ~(BIT0 | BIT2);
+        P2OUT ^= BIT1;
+    }
+
+    if(alarmState == 1){
+
+        P2OUT &= ~(BIT1 |BIT2);
+        dutyBuzz = buzzCount*100;
+        P2OUT ^= BIT(0);
+        buzzCount++;
+        if(buzzCount == 2) buzzCount = 0;
+
+
+    }
+
+    if(alarmState == 2){
+        P2OUT &= ~(BIT1 |BIT2);
+        P2OUT = BIT0;
+        buzzCount++;
+        dutyBuzz = buzzCount*25+50;
+        if(buzzCount == 2) buzzCount = 0;
+    }
+
+    LED_PWM_SCREEN_BUZZER(duty,dutyBuzz);
 
     Timer32_clearInterruptFlag(TIMER32_0_BASE);
     Timer32_setCount(TIMER32_0_BASE,20000);
 }
 
 
-void LED_PWM(int dutyCycle){
+void LED_PWM_SCREEN_BUZZER(int dutyCycleScreen, int dutyCycleBuzzer){
     // Output is on P7.7
-    P7->DIR |= BIT7;
-    P7->SEL0 |= BIT7;
-    P7->SEL1 &= ~BIT7;
-    
     TIMER_A1->CCR[0] = 100 - 1;
     TIMER_A1-> CCTL[1] = TIMER_A_CCTLN_OUTMOD_7;
-    TIMER_A1->CCR[1] = dutyCycle;
+    TIMER_A1->CCR[1] = dutyCycleScreen;
+    TIMER_A1-> CCTL[2] = TIMER_A_CCTLN_OUTMOD_7;
+    TIMER_A1->CCR[2] = dutyCycleBuzzer;
+
     TIMER_A1-> CTL = TIMER_A_CTL_SSEL__SMCLK | TIMER_A_CTL_MC__UP | TIMER_A_CTL_CLR;
 }
+
 
 void sysTimerInit(){
     SysTick -> CTRL = 0;            // disable SysTick during setup
@@ -163,3 +205,61 @@ void T32Init(){
     MAP_Timer32_startTimer(TIMER32_BASE, true);
 
 }
+
+
+
+void playPeriod(int T){
+    P2->OUT = BIT7;                    // Blink P1.0 LED
+    sysDelay(10);
+    P2->OUT = ~BIT7;
+    sysDelay(T-10);
+}
+
+/* note   frequency     period  timeHigh
+* c            261 Hz          3830    1915
+* d            294 Hz          3400    1700
+* e            329 Hz          3038    1519
+* f            349 Hz          2864    1432
+* g            392 Hz          2550    1275
+* a            440 Hz          2272    1136
+* b            493 Hz          2028    1014
+* C            523 Hz          1912    956
+*
+* http://www.arduino.cc/en/Tutorial/Melody
+*/
+void playNote(int note){
+
+    if(note == 0) playPeriod(4540); //A
+    if(note == 1) playPeriod(4290); //A#
+    if(note == 2) playPeriod(4050); //B
+    if(note == 3) playPeriod(3831); //C
+    if(note == 4) playPeriod(3610); //C#
+    if(note == 5) playPeriod(3400); //D
+    if(note == 6) playPeriod(3210); //D#
+    if(note == 7) playPeriod(3038); //E
+    if(note == 8) playPeriod(2864); //F
+    if(note == 9) playPeriod(2700); //F#
+    if(note == 10) playPeriod(2550); //G
+    if(note == 11) playPeriod(2410); //G#
+}
+
+void playBlank(int T){
+    P2->OUT = ~BIT7;
+    sysDelay(T);
+}
+
+void note(int note){
+    int i = 0;
+    for(i ; i<100+note ; i++)
+        playNote(note);
+    playBlank(7000000);
+
+}
+
+void quarterRest(){
+    int i = 0;
+    for(i ; i<100+note ; i++)
+        playBlank(300);
+}
+
+
